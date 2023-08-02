2a9f9d4ec4bd158a4796f5dbdaa76167fe5ecf67
Ramsyz <seetaram.vannemreddi@gmail.com>
2023-08-02 08:48:02 -0400

    modifed styles file and Added product.js file

---
+++ products.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {
+    name: "Basic Tee",
+    price: 3800,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979055/xzdukv3oxbnqm0hmq8sv.jpg",
+  },
+  {
+    name: "Basic short",
+    price: 4800,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979054/zbs6vch7luyxr2ee6kt6.jpg",
+  },
+  {
+    name: "Shoes",
+    price: 65000,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979055/dne3fmvbyeaidrrnqi4n.jpg",
+  },
+  {
+    name: "Bag",
+    price: 5000,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979054/eqlthgfl1qxzre1kpqqv.jpg",
+  },
+  {
+    name: "Bottle",
+    price: 3000,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979055/u5hwfm6hv3wvfc45mbc5.jpg",
+  },
+  {
+    name: "Gloves",
+    price: 5500,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979054/od7lbrq1e2chqqwzyd5a.jpg",
+  },
+  {
+    name: "Headphones",
+    price: 12900,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979054/od7lbrq1e2chqqwzyd5a.jpg",
+  },
+  {
+    name: "Perfume",
+    price: 8500,
+    currency: "USD",
+    image:
+      "https://res.cloudinary.com/dqntawt3s/image/upload/v1690979054/od7lbrq1e2chqqwzyd5a.jpg",
+  },
+];
