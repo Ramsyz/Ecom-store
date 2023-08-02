283b3161ed40eefea9a34a9423fd0828d372a6fa
Ramsyz <seetaram.vannemreddi@gmail.com>
2023-08-02 11:25:41 -0400

    header footer meta logo completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="border-t border-gray-100 py-10 text-center">
+      <p className="text-sm">&#169; 2023 EcomStore. All rights reserved</p>
+    </footer>
+  )
+}
